a449dbaa33d60cdb551b0774726886e6a636f8d5
Mohamed6247 <codnetwork20@gmail.com>
2023-03-16 15:26:56 -0700

    puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual
+ * main - prints with proper grammar, but the outcome is a piece of art,
  * puzzle, followed by a newline."
  * Return: Always 0.
  */
@@ -12,6 +12,6 @@ int main(void)
 
 
 {
-        puts("with proper grammar, but the outcome is a piece of art,");
+        printf("with proper grammar, but the outcome is a piece of art,");
         return (0);
 }
