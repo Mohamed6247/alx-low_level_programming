3a20c333bfc0041c0f5978f9645e886454b4e5aa
Mohamed6247 <codnetwork20@gmail.com>
2023-03-16 16:22:16 -0700

    puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - Entry point 
- *
- * Return: Always 1 (Success).
- */
-int main(void) 
-{ 
-write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
-
+* main - A c program that prints a line to the standard error
+* Return: Always 1 (Success)
+*/
+int main(void)
+{
+fput("and that piece of art useful\" - Dora Kor par, 2015-10-19\n", stdout);
 return (1);
 }
